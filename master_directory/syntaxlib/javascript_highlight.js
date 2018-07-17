bf22cd57d7b8fdbd4bee5dbb819ce80108d7aa61
TwixKitty <rybothecamel@gmail.com>
2018-07-16 20:59:02 -0400

    Number work

--- master_directory/syntaxlib/javascript_highlight.js
+++ master_directory/syntaxlib/javascript_highlight.js
@@ -86,6 +86,7 @@ parseRoutines.push({ reg: new RegExp("(undefined)"), replacement: "<span class='
 parseRoutines.push({ reg: new RegExp("(case)"), replacement: "<span class='javascript_case'>$1</span>" });
 parseRoutines.push({ reg: new RegExp("(break)"), replacement: "<span class='javascript_break'>$1</span>" });
 parseRoutines.push({ reg: new RegExp("(function)"), replacement: "<span class='javascript_function'>$1</span>" });
-parseRoutines.push({ reg: new RegExp("\\b(?![&#35;])(\\d+)"), replacement: "<span class='javascript_number'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("\\b(\\d+)(?!;)\\b"), replacement: "<span class='javascript_number'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("( \\d+)"), replacement: "<span class='javascript_number'>$1</span>" });
 var highlighter = new Syntax_Highlighter(parseRoutines);
 highlighter.parseAllSnippets();
