0bc30eba3cf0c3c309de8fcfc5b8d9c82bc66609
TwixKitty <rybothecamel@gmail.com>
2018-08-04 17:20:26 -0400

    Added switch support

--- master_directory/syntaxlib/javascript_highlight.js
+++ master_directory/syntaxlib/javascript_highlight.js
@@ -85,6 +85,7 @@ parseRoutines.push({ reg: new RegExp("(return)"), replacement: "<span class='jav
 parseRoutines.push({ reg: new RegExp("(if|for|else)"), replacement: "<span class='javascript_condition'>$1</span>" });
 parseRoutines.push({ reg: new RegExp("(undefined)"), replacement: "<span class='javascript_undefined'>$1</span>" });
 parseRoutines.push({ reg: new RegExp("(case)"), replacement: "<span class='javascript_case'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(switch)"), replacement: "<span class='javascript_switch'>$1</span>" });
 parseRoutines.push({ reg: new RegExp("(break)"), replacement: "<span class='javascript_break'>$1</span>" });
 parseRoutines.push({ reg: new RegExp("(function)"), replacement: "<span class='javascript_function'>$1</span>" });
 parseRoutines.push({ reg: new RegExp("\\b(\\d+)(?!;)\\b"), replacement: "<span class='javascript_number'>$1</span>" });
