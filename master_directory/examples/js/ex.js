46bcd1c344f06c309a4c65eff271ad65b7508984
TwixKitty <rybothecamel@gmail.com>
2019-11-14 21:39:15 -0500

    Added rotation and face shading to the rasterizer

--- master_directory/examples/js/ex.js
+++ master_directory/examples/js/ex.js
@@ -1,4 +1,4 @@
-var cube = [
+var orig_cube = [
         //front face
         [-1, -1, 1, 1],
         [1, -1, 1, 1],
@@ -98,41 +98,103 @@ function scale(x, y, z)
     worldMatrix = math.multiply(scaleMatrix, worldMatrix);
 }
 
-translate(1, 0, -3);
+translate(0, 0, -4);
 
-for(var i = 0;i < cube.length;i++)
-{
-    cube[i] = math.multiply(worldMatrix, cube[i]);
-    cube[i] = math.multiply(perspectiveMatrix, cube[i]);
-    cube[i] = math.divide(cube[i], cube[i][3]);
-    cube[i] = math.multiply(cube[i], 256);
-    cube[i] = math.add(cube[i], 256);
+// draws a shaded face based on a passed in id for that particular face
+function draw_face(face_id) {
+	switch(face_id) {
+		case 0:
+			ctx.fillStyle = "#F00";
+			ctx.beginPath();
+			ctx.moveTo(cube[0][0], cube[0][1]);
+			ctx.lineTo(cube[1][0], cube[1][1]);
+			ctx.lineTo(cube[2][0], cube[2][1]);
+			ctx.lineTo(cube[3][0], cube[3][1]);
+			ctx.lineTo(cube[0][0], cube[0][1]);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+		break;
+		case 1:
+			ctx.fillStyle = "#00F";
+			//draws back face
+			ctx.beginPath();
+			ctx.moveTo(cube[4][0], cube[4][1]);
+			ctx.lineTo(cube[5][0], cube[5][1]);
+			ctx.lineTo(cube[6][0], cube[6][1]);
+			ctx.lineTo(cube[7][0], cube[7][1]);
+			ctx.lineTo(cube[4][0], cube[4][1]);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+		break;
+		case 2:
+			ctx.fillStyle = "#0F0";
+			ctx.beginPath();
+			ctx.moveTo(cube[0][0], cube[0][1]);
+			ctx.lineTo(cube[3][0], cube[3][1]);
+			ctx.lineTo(cube[7][0], cube[7][1]);
+			ctx.lineTo(cube[4][0], cube[4][1]);
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+		break;
+		case 3:
+			ctx.fillStyle = "#FF0";
+			ctx.beginPath();
+
+			ctx.moveTo(cube[1][0], cube[1][1]);
+			ctx.lineTo(cube[2][0], cube[2][1]);
+			ctx.lineTo(cube[6][0], cube[6][1]);
+			ctx.lineTo(cube[5][0], cube[5][1]);
+
+			ctx.closePath();
+			ctx.fill();
+			ctx.stroke();
+		break;
+		default:
+		alert("BAD FACEID");
+		break;
+	}
 }
 
-//draw front face
-ctx.beginPath();
-ctx.moveTo(cube[0][0], cube[0][1]);
-ctx.lineTo(cube[1][0], cube[1][1]);
-ctx.lineTo(cube[2][0], cube[2][1]);
-ctx.lineTo(cube[3][0], cube[3][1]);
-ctx.lineTo(cube[0][0], cube[0][1]);
-ctx.stroke();
-//draws back face
-ctx.beginPath();
-ctx.moveTo(cube[4][0], cube[4][1]);
-ctx.lineTo(cube[5][0], cube[5][1]);
-ctx.lineTo(cube[6][0], cube[6][1]);
-ctx.lineTo(cube[7][0], cube[7][1]);
-ctx.lineTo(cube[4][0], cube[4][1]);
-ctx.stroke();
-//connects front face to back face
-ctx.beginPath();
-ctx.moveTo(cube[0][0], cube[0][1]);
-ctx.lineTo(cube[4][0], cube[4][1]);
-ctx.moveTo(cube[1][0], cube[1][1]);
-ctx.lineTo(cube[5][0], cube[5][1]);
-ctx.moveTo(cube[2][0], cube[2][1]);
-ctx.lineTo(cube[6][0], cube[6][1]);
-ctx.moveTo(cube[3][0], cube[3][1]);
-ctx.lineTo(cube[7][0], cube[7][1]);
-ctx.stroke();
+// draws a rotating cube
+function draw_rotate_cube() {
+	ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+	rotate(0, 1, 0, 0.1);
+	translate(Math.sin(0.4), 0, 0);
+
+	cube = orig_cube.slice(); // creates a copy of the original cube
+
+	// transforms the cube by the worldmatrix, perspectiveMatrix, and divides it by the 4th vector to correctly render it
+	// multiplies and adds 256 for better display
+	for(var i = 0;i < cube.length;i++)
+	{
+		cube[i] = math.multiply(worldMatrix, cube[i]);
+		cube[i] = math.multiply(perspectiveMatrix, cube[i]);
+		cube[i] = math.divide(cube[i], cube[i][3]);
+		cube[i] = math.multiply(cube[i], 256);
+		cube[i] = math.add(cube[i], 256);
+	}
+	
+	// calculates the length of all of the faces
+	var face1 = Math.abs(cube[3][1] - cube[0][1]);
+	var face2 = Math.abs(cube[7][1] - cube[4][1]);
+
+	var line_1 = Math.abs(cube[7][1] - cube[0][1]);
+	var line_2 = Math.abs(cube[2][1] - cube[1][1]);
+	
+	var face_array = [[face1, 0], [face2, 1], [line_1, 2], [line_2, 3]];
+	
+	// sorts all of the faces by how close they are to the camera based on how long they are on the y axis
+	var sorted_faces = face_array.sort((a, b) => a[0] - b[0]);
+	
+	// draws the faces in order, shortest is drawn first, then longer are drawn over it
+	for(var i = 0;i < sorted_faces.length;i++) {
+		draw_face(sorted_faces[i][1]);
+	}
+}
+
+// rotates the cube every 0.1 seconds
+setInterval(draw_rotate_cube, 100);
