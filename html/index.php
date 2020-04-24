cdfaeb5406090d8b7f1dd56b88ee100f12c1f80d
TwixKitty <rybothecamel@gmail.com>
2020-04-24 12:04:31 -0400

    New nginx code

---
+++ html/index.php
@@ -0,0 +1,46 @@
+<?php
+
+# phrases for the pulsar
+$phrases = file_get_contents("site_phrases.txt");
+$phrase_list = explode("\n", $phrases);
+$phrase_pick = rand(0, count($phrase_list) - 1);
+$phrase = $phrase_list[$phrase_pick];
+
+$page_ref = $_GET["_url"];
+
+if($page_ref == "") {
+  $page_ref = "/startpage";
+}
+
+$template_root = 'templates';
+$page_root = 'guts';
+
+$php_watch = @file_get_contents("$template_root$page_ref.html");
+
+if($php_watch === false) {
+  header("HTTP/1.0 404 Not Found");
+  echo file_get_contents("$page_root/404.html");
+} else {
+  while(preg_match("/%%.+%%/", $php_watch, $match_a) === 1) {
+    $match = $match_a[0];
+    
+    $file_location = str_replace("%%", "", $match);
+    $file_location = preg_replace("/(\.\.\/)/", "", $file_location);
+    $match_file_contents = @file_get_contents($page_root."/".$file_location);
+    
+    if($match_file_contents === false) {
+      $php_watch = str_replace($match, "<b style='color:red'>COULDN'T LOAD PATTERN $file_location</b>", $php_watch);
+    } else {
+      $php_watch = str_replace($match, $match_file_contents, $php_watch);
+    }
+  }
+  
+  $php_watch = str_replace("@random_phrase", $phrase, $php_watch);
+  echo $php_watch;
+}
+# footer generation happens here
+
+echo "<hr>";
+
+echo "<div style='text-align:center'>&xi; This page was generated " . date("D M d, Y G:i") . "</div>";
+?>
---
+++ html/syntaxlib/javascript_highlight.js
@@ -0,0 +1,96 @@
+var Syntax_Highlighter = /** @class */ (function () {
+    function Syntax_Highlighter(routines) {
+        this.parseRoutines = routines;
+    }
+    Syntax_Highlighter.prototype.parseSingleResult = function (s, r, replacement) {
+        if (s.match(r) == null) {
+            return [{ value: s, grouped: false }];
+        }
+        var match_len = s.match(r)[0].length;
+        var str1 = s.slice(0, s.search(r));
+        var str2 = s.slice(s.search(r), s.search(r) + match_len).replace(r, replacement);
+        var str3 = s.slice(s.search(r) + match_len);
+        var strarr = [{ value: str1, grouped: false }, { value: str2, grouped: true }, { value: str3, grouped: false }];
+        return strarr;
+    };
+    /*
+    * Takes a snippet
+    * Returns the snippet parsed
+    */
+    Syntax_Highlighter.prototype.parseSnippet = function (results, r, replacement) {
+        if (results.length == 1) {
+            results = this.parseSingleResult(results[0].value, r, replacement);
+        }
+        for (var i = 0; i < results.length; i++) {
+            if (results[i].grouped == false && results[i].value.search(r) != -1) {
+                var new_results = this.parseSingleResult(results[i].value, r, replacement);
+                results.splice.apply(results, [i, 1].concat(this.parseSnippet(new_results, r, replacement)));
+            }
+        }
+        return results;
+    };
+    Syntax_Highlighter.prototype.parseAllRoutines = function (s, routines) {
+        var results = [{ value: s, grouped: false }];
+        for (var i = 0; i < routines.length; i++) {
+            results = this.parseSnippet(results, routines[i].reg, routines[i].replacement);
+        }
+        // join all of the results into a friendly routine
+        var str = "";
+        for (var i = 0; i < results.length; i++) {
+            str += results[i].value;
+        }
+        return str;
+    };
+    Syntax_Highlighter.prototype.escapeHtml = function (string) {
+        return String(string).replace(/[&<>"'`=\/]/g, function (s) {
+            return Syntax_Highlighter.entityMap[s];
+        });
+    };
+    Syntax_Highlighter.prototype.parseAllSnippets = function () {
+        var code_snippets = document.getElementsByClassName("code");
+        for (var i = 0; i < code_snippets.length; i++) {
+            var escaped = this.escapeHtml(code_snippets[i].innerHTML);
+            var t = this.parseAllRoutines(escaped, this.parseRoutines);
+            var pre = document.createElement("pre");
+            pre.innerHTML = t;
+            code_snippets[i].parentNode.appendChild(pre);
+        }
+        var len = code_snippets.length;
+        for (var i = 0; i < len; i++) { // cleanup
+            code_snippets[0].parentNode.removeChild(code_snippets[0]);
+        }
+    };
+    /*
+    * Function to escape html brackets so it doesn't interfere with the pre tag
+    */
+    Syntax_Highlighter.entityMap = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#39;',
+        '/': '&#x2F;',
+        '`': '&#x60;',
+        '=': '&#x3D;'
+    };
+    return Syntax_Highlighter;
+}());
+/// <reference path='syntax.ts'/>
+var parseRoutines = new Array();
+parseRoutines.push({ reg: new RegExp("(&#x2F;&#x2F;.*)"), replacement: "<span class='javascript_comment'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(&#x2F;\\*[^*]*\\*+(?:[^&#x2F;*][^*]*\\*+)*&#x2F;)"), replacement: "<span class='javascript_multi_comment'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(&quot;|&#39;)(.*?)(&quot;|&#39;)"), replacement: "<span class='javascript_string'>$1$2$3</span>" });
+parseRoutines.push({ reg: new RegExp("(var)"), replacement: "<span class='javascript_type'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(return)"), replacement: "<span class='javascript_return'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(if|for|else)"), replacement: "<span class='javascript_condition'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(undefined)"), replacement: "<span class='javascript_undefined'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(case)"), replacement: "<span class='javascript_case'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(switch)"), replacement: "<span class='javascript_switch'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(break)"), replacement: "<span class='javascript_break'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("(function)"), replacement: "<span class='javascript_function'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("0x([\\dABCDEF]+)"), replacement: "<span class='javascript_number'>0x$1</span>" });
+parseRoutines.push({ reg: new RegExp("\\b(\\d+)(?!;)\\b"), replacement: "<span class='javascript_number'>$1</span>" });
+parseRoutines.push({ reg: new RegExp("\\.(\\d+)"), replacement: "<span>.</span><span class='javascript_number'>$1</span>" });
+parseRoutines.push({ reg: new RegExp(" (-?)(\\d+)"), replacement: "<span> $1</span><span class='javascript_number'>$2</span>" });
+var highlighter = new Syntax_Highlighter(parseRoutines);
+highlighter.parseAllSnippets();
